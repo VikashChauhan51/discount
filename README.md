9d13a06a3818f15cf623393fb5dbc50762f6ebd8
Vikash Chauhan <vikashchauhan51@gmail.com>
2024-06-07 16:19:32 +0530

    Added connection

--- README.md
+++ README.md
@@ -7,3 +7,22 @@
 ## Use Grpc UI
 - `grpcui -plaintext localhost:<service_port>`.
 - `grpcui -plaintext localhost:12345`.
+
+
+## Connect Diccount
+The Discount gRPC service is running on two ports: `7299` (HTTPS) and `5155` (HTTP).
+
+You can connect to the HTTP port by using the following command:
+
+```shell
+grpcui -plaintext localhost:5155
+```
+
+If you want to connect to the HTTPS port (`7299`), you might need to provide the certificate details. Here's an example:
+
+```shell
+grpcui -cacert /path/to/cert.pem localhost:7299
+```
+
+Replace `/path/to/cert.pem` with the path to your certificate file.
+
--- src/Discount.Grpc/Program.cs
+++ src/Discount.Grpc/Program.cs
@@ -3,12 +3,19 @@ using Discount.Grpc.Services.v1;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddGrpc();
-
+builder.Services.AddGrpc(options =>
+{
+    options.EnableDetailedErrors = true;
+});
+builder.Services.AddGrpcReflection();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<DiscountService>();
+
+// Enable gRPC reflection
+app.MapGrpcReflectionService();
+
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
